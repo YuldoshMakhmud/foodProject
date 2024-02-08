08a3d867d93b56ae57b628299076632e849e14a8
YuldoshMakhmud <154692684+YuldoshMakhmud@users.noreply.github.com>
2024-02-09 04:21:49 +0900

    section1

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ setInterval(() => {
     i++
     const icon =document.querySelector('.section-1-icons .change')
     icon.classList.remove('change')
+    if(i>icons.length){
+        icons[0].classList.add('change')
+        i=1
+    }else{
 
+        icon.nextElementSibling.classList.add('change')
+    }
 
-    icon.nextElementSibling.classList.add('change')
-}, 1000);
+}, 4000);
